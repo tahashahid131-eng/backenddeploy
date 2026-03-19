752e6435197abddf2c068497f0c0dea79b827eeb
tahashahid131-eng <tahashahid131@gmail.com>
2026-03-20 03:38:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/x',(req, res) =>{
+    res.send('being_jinu')
+})
+app.get('/login',(req, res)=>{
+    res.send('<h1>please login at chai aur code </h1>')
+})
+app.get('/youtube',(req, res)=>{
+    res.send('<h2> chai aur code </h2>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
